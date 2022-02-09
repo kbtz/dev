808504c789fe9825c99d9a63cd20d6c5ed7d0268
kbtz <github@kbtz.dev>
2022-02-08 23:34:09 -0300

    build icons into json

--- env/icons/build.bash
+++ env/icons/build.bash
@@ -1,25 +1,62 @@
 #!/bin/bash
 # uses inkscape, 7z and svgo
-set -e
 
-SRC=vectors
+SET=menu
+PWD0=${PWD}
 CWD=${BASH_SOURCE%/*}
-OUT=$(realpath ./src/file/icons)
+SRC=$(realpath ${CWD}/${SET}.svg)
+OUT=$(realpath ./src/file)/${SET}.icons.json
 
 cd ${CWD}
-rm -rf ${SRC}; mkdir ${SRC}
-rm -rf ${OUT}; mkdir ${OUT}
+set -e
 
-inkscape --export-type=tar ${SRC}.svg 
+rm -rf ${SET}; mkdir ${SET}
+inkscape --export-type=tar ${SRC}
 
 set +e
-7z x -o${SRC} ${SRC}.tar; rm ${SRC}.tar
+7z x -o${SET} ${SET}.tar; rm ${SET}.tar
+
+cd ${SET}
+set -e
+
+cat <<JSON > svgo.config.json
+{
+	"plugins": [
+		{
+			"name": "preset-default",
+			"params": {
+				"overrides": {
+					"removeViewBox": false
+				}
+			}
+		}
+	]
+}
+JSON
 
 svgo \
-	--config svgo.config.js \
-	--folder ${SRC} \
-	--output ${OUT} \
+	--folder . \
 	--precision 0 \
 	--pretty \
 	--indent 2 \
 	--final-newline
+
+node <<JS > ${OUT}
+const
+	body = {
+		viewBox: $(sed -nE 's,.*viewBox=("[^"]*").*,\1,p' ${SRC}),
+		paths: {
+			$(for ICON in *.svg; do
+				echo ${ICON%.*}: [ \
+				 $(sed -nE 's,.* d=("[^"]*").*,\1\,,p' ${ICON}) \
+				],
+			done)
+		}
+	},
+	output = JSON.stringify(body, null, 2)
+
+console.log(output)
+JS
+
+cd ..; rm -r ${SET}
+cd ${PWD0}
