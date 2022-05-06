b5880a1eab2a71706cd2a2ad880dffe672295167
kbtz <github@kbtz.dev>
2022-05-06 11:47:11 -0300

    pointer hex morph

--- src/comp/Pointer.vue
+++ src/comp/Pointer.vue
@@ -5,22 +5,56 @@ import { ref } from 'vue';
 const
 	point = ref<HTMLElement>(null as 𝞌),
 	ring = ref<HTMLElement>(null as 𝞌),
+	focused = {
+		element: null as Element | null,
+		morph: ref(false),
+		scale: 1, x: 0, y: 0
+	},
 	size = 24
 
 function pointermove({ isPrimary, pageX: x, pageY: y }: PointerEvent) {
 	if (!isPrimary) return
 
 	point.value.style.transform = `translate(${x}px, ${y}px)`
-	setTimeout(() => {
-		ring.value.style.transform = `translate(${x}px, ${y}px)`
-	}, 20)
+	after(.02, () => ring.value.style.transform =
+		focused.element
+			? `translate(${focused.x}px, ${focused.y}px) scale(${focused.scale})`
+			: `translate(${x}px, ${y}px) scale(1)`
+	)
+
+	if (state.backdrop.cover) return
+	const element = document.elementFromPoint(x, y)
+	if (!element) throw 'wat'
+	if (element == focused.element) return
+	if (element.classList.contains('focusable')) focus(element)
+	else blur()
 }
+
+function focus(element: Element) {
+	const
+		{ top, left, width, height } = element.getBoundingClientRect(),
+		scale = Math.max(width, height) / size
+	focused[𝞏] = {
+		element, scale,
+		x: left + width / 2,
+		y: top + height / 2,
+	}
+
+	focused.morph.value = true
+}
+
+function blur() {
+	if (!focused.element) return
+	focused.element = null
+	focused.morph.value = false
+}
+
 </script>
 
 <template>
-	<div class="pointer" v-page="{ pointermove }" :style="`--base-size:${size}px`">
+	<div class="pointer" v-page="{ pointermove }" :style="`--size-base:${size}px`">
+		<div ref="ring" class="ring" :class="{ morph: focused.morph.value }" />
 		<div ref="point" class="point" />
-		<div ref="ring" class="ring" />
 	</div>
 </template>
 
@@ -45,15 +79,23 @@ function pointermove({ isPrimary, pageX: x, pageY: y }: PointerEvent) {
 	}
 
 	.point {
-		--size: calc(0.4 * var(--base-size));
+		--size: calc(0.4 * var(--size-base));
 		background: var(--gray);
 	}
 
 	.ring {
-		--size: var(--base-size);
+		--size: var(--size-base);
 		transition: all 80ms ease-out;
 		border: 2px solid var(--gray);
 		box-sizing: border-box;
+		backdrop-filter: invert(0);
+
+		&.morph {
+			backdrop-filter: invert(1);
+			border-width: 0.1px;
+			border-color: var(--light);
+			-webkit-mask-box-image: url(hex.png);
+		}
 	}
 }
 </style>
